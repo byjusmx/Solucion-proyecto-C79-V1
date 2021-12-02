08e5524177ed213022d2df7b8a42384cf473563a
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-12-01 21:00:58 -0600

    Update DailyPic.js

--- screens/DailyPic.js
+++ screens/DailyPic.js
@@ -46,7 +46,7 @@ export default class DailyPicScreen extends Component {
             return (
                 <View
                     style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                    <Text>Loading</Text>
+                    <Text>Cargando</Text>
                 </View>
             )
         }
@@ -56,11 +56,11 @@ export default class DailyPicScreen extends Component {
                     <SafeAreaView style={styles.droidSafeArea} />
                     <ImageBackground source={require('../assets/stars.gif')} style={styles.backgroundImage}>
                         <View style={{ flex: 0.15, justifyContent: "center", textAlign: "center" }}>
-                            <Text style={styles.routeText}>Daily Pic</Text>
+                            <Text style={styles.routeText}>Imágenes diarias</Text>
                         </View>
                         <ScrollView style={styles.listContainer}>
                             <TouchableOpacity
-                                onPress={() => Linking.openURL(this.state.apod.url).catch(err => console.error("Couldn't load page", err))}
+                                onPress={() => Linking.openURL(this.state.apod.url).catch(err => console.error("No se pudo cargar la página", err))}
                             >
                                 <Image source={{ "uri": url }} style={{ width: "100%", height: 300, borderRadius: 10 }}></Image>
                             </TouchableOpacity >
